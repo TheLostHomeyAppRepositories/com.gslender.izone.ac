3638741d64ab2c939672b64079f6415d62fb1e3e
Grant Slender <gslender@gmail.com>
2025-01-02 14:35:28 +1000

    strip http down to just using node net

--- app.js
+++ app.js
@@ -2,72 +2,46 @@
 
 const Homey = require('homey');
 const dgram = require('dgram');
-const axios = require('axios');
-const http = require('http');
-
-// Create custom HTTP and HTTPS agents with keepAlive set to false
-const httpAgent = new http.Agent({ keepAlive: false });
-
-// Configure Axios instance with these agents
-const axiosInstance = axios.create({
-  httpAgent,
-  timeout: 5000,  // Optional timeout for requests
-});
+const net = require('net');
 
 function isValidIPAddress(ipaddress) {
-  // Check if ipaddress is undefined or null
-  if (ipaddress === undefined || ipaddress === null) {
-    return false; // Not a valid IP address
-  }
-
-  // Regular expression for IPv4 validation
+  if (!ipaddress) return false; // Check for undefined or null
   const ipPattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-
-  // Validate IP address pattern
   return ipPattern.test(ipaddress);
 }
 
 class iZoneApp extends Homey.App {
-
   async onInit() {
     this.log(`${this.id} running...`);
-
-    // uncomment only for testing !!
-    // this.homey.settings.unset('izone.ipaddress');
     this.enableRespDebug = true;
-
     this.updateSettings();
 
     if (!isValidIPAddress(this.ipaddress)) {
-      this.homey.app.sendMessageAndReturnAddress()
-        .then(address => {
-          this.log('Remote address:', address);
-          this.homey.settings.set('izone.ipaddress', address);
-          this.ipaddress = address;
-        })
-        .catch(error => {
-          this.error('Error occurred:', error);
-        });
+      try {
+        const address = await this.sendMessageAndReturnAddress();
+        this.log('Remote address:', address);
+        this.homey.settings.set('izone.ipaddress', address);
+        this.ipaddress = address;
+      } catch (error) {
+        this.error('Error occurred:', error);
+      }
     }
 
-    this.state = {};
-    this.state.ac = {};
-    this.state.ac.zones = {};
+    this.state = { ac: { zones: {} } };
 
-    // getFirmwareList
-    let resultFmw = await this.getFirmwareList();
+    const resultFmw = await this.getFirmwareList();
+    this.log('Firmware List:', resultFmw);
+
     if (resultFmw.status === "ok") {
       this.state.firmware = resultFmw.Fmw;
-
       this.isRunning = true;
-      await this.refreshPolling(2000); // start 2 second after init
+      await this.refreshPolling(2000);
 
       this.homey.settings.on('set', this.onSettingsChanged.bind(this));
     }
   }
 
-  async refreshPolling(delay) {
-    delay = delay || 0;
+  async refreshPolling(delay = 0) {
     this.homey.clearInterval(this.pollingID);
     this.homey.setTimeout(async () => {
       await this.refresh();
@@ -78,7 +52,7 @@ class iZoneApp extends Homey.App {
   }
 
   async onSettingsChanged(key) {
-    if (key === 'izone.polling' || key === 'izone.ipaddress') {
+    if (['izone.polling', 'izone.ipaddress'].includes(key)) {
       this.updateSettings();
       this.homey.setTimeout(async () => {
         await this.refreshPolling();
@@ -89,39 +63,32 @@ class iZoneApp extends Homey.App {
 
   async updateSettings() {
     this.ipaddress = this.homey.settings.get('izone.ipaddress');
-
     const MIN_POLLING_INTERVAL = 15000;
     const MAX_POLLING_INTERVAL = 300000;
-
-    let pollingInterval = parseInt(this.homey.settings.get('izone.polling'), 10);
-
-    if (typeof pollingInterval !== 'number' || isNaN(pollingInterval)) {
-      pollingInterval = MIN_POLLING_INTERVAL; // Default value if not a number or undefined
-    } else {
-      pollingInterval = Math.max(MIN_POLLING_INTERVAL, Math.min(MAX_POLLING_INTERVAL, pollingInterval));
-    }
-
+    const pollingInterval = Math.max(
+      MIN_POLLING_INTERVAL,
+      Math.min(
+        MAX_POLLING_INTERVAL,
+        parseInt(this.homey.settings.get('izone.polling'), 10) || MIN_POLLING_INTERVAL
+      )
+    );
     this.pollingInterval = pollingInterval;
+
     this.log('Remote address:', this.ipaddress);
     this.log('Polling interval:', this.pollingInterval);
   }
 
-  async onUninit() {
-    this.isRunning = false;
-  }
-
   async refresh() {
-    // starting or repeating, so do getAcSystemInfo 
     let failed = false;
-    let result = await this.getAcSystemInfo();
+    const result = await this.getAcSystemInfo();
     if (result.status === "ok") {
-      this.state.ac.sysinfo = result.SystemV2
+      this.state.ac.sysinfo = result.SystemV2;
       this.updateCapabilitiesDeviceId('ac.sysInfo');
 
       for (let zoneNum = 0; zoneNum < result.SystemV2.NoOfZones; zoneNum++) {
         const resultZone = await this.getZonesInfo(zoneNum);
         if (resultZone.status === "ok") {
-          let zoneIdx = "zone" + resultZone.ZonesV2.Index;
+          const zoneIdx = `zone${resultZone.ZonesV2.Index}`;
           this.state.ac.zones[zoneIdx] = resultZone.ZonesV2;
           this.updateCapabilitiesDeviceId(zoneIdx);
         } else {
@@ -135,7 +102,6 @@ class iZoneApp extends Homey.App {
   }
 
   async updateCapabilitiesDeviceId(id) {
-    // update the drivers and devices
     const drivers = this.homey.drivers.getDrivers();
     for (const driver in drivers) {
       const devices = this.homey.drivers.getDriver(driver).getDevices();
@@ -143,14 +109,12 @@ class iZoneApp extends Homey.App {
         if (device.getData().id === id && device.updateCapabilities) {
           await device.setAvailable();
           await device.updateCapabilities();
-          break;
         }
       }
     }
   }
 
   async setDevicesUnavailable() {
-    // update the drivers and devices
     const drivers = this.homey.drivers.getDrivers();
     for (const driver in drivers) {
       const devices = this.homey.drivers.getDriver(driver).getDevices();
@@ -160,134 +124,92 @@ class iZoneApp extends Homey.App {
     }
   }
 
-  async getAcSystemInfo() {
-    if (!isValidIPAddress(this.ipaddress)) return {};
-    const uri = `http://${this.ipaddress}:80/iZoneRequestV2`;
-    if (this.enableRespDebug) this.log(`getAcSystemInfo() ${uri}`);
-    let respData = {};
+  async sendRawRequest(path, body) {
+    if (!isValidIPAddress(this.ipaddress)) return { status: 'failed: invalid IP address' };
+    const host = this.ipaddress;
+    const port = 80;
 
-    const mapBody = { "iZoneV2Request": { "Type": 1, "No": 0, "No1": 0 } };
+    const request = `POST ${path} HTTP/1.1\r\n\r\n` +
+      body;
 
-    try {
-      respData.status = "failed";
-      const response = await axiosInstance.post(uri, JSON.stringify(mapBody), {
-        headers: {
-          'Content-Type': 'application/json'
+    return new Promise((resolve, reject) => {
+      const client = net.createConnection({ host, port }, () => {
+        if (this.enableRespDebug) this.log(`Sending request:\n${request}`);
+        client.write(request);
+      });
+
+      let responseData = '';
+      client.on('data', (chunk) => {
+        responseData += chunk.toString();
+      });
+
+      client.on('end', () => {
+        try {
+          const [headers, body = ''] = responseData.split('\r\n\r\n');
+          const statusCode = parseInt(headers.split(' ')[1], 10);
+          if (statusCode === 200) {
+            if (body.trim()) {
+              try {
+                // Try to parse the body as JSON
+                resolve({ ...JSON.parse(body), status: 'ok' });
+              } catch (jsonErr) {
+                resolve({ status: 'ok', body: body.trim() });
+              }
+            } else {
+              // No body in the response
+              resolve({ status: 'ok' });
+            }
+          } else {
+            resolve({ status: `failed: HTTP ${statusCode}` });
+          }
+        } catch (err) {
+          this.log(err.stack); // Dump the stack trace to log
+          reject(`Failed to parse response: ${err}`);
         }
       });
-      respData = response.data;
-      if (respData.hasOwnProperty("SystemV2")) respData.status = "ok";
-    } catch (e) {
-      if (this.enableRespDebug) this.log(`getAcSystemInfo() ERROR: ${e}`);
-      respData.status = "failed: " + e;
-    }
-    return respData;
+
+      client.on('error', (err) => {
+        resolve({ status: `failed: ${err.message}` });
+      });
+    });
+  }
+
+  async getAcSystemInfo() {
+    const body = JSON.stringify({ iZoneV2Request: { Type: 1, No: 0, No1: 0 } });
+    return this.sendRawRequest('/iZoneRequestV2', body);
   }
 
   async getZonesInfo(zone) {
-    if (!isValidIPAddress(this.ipaddress)) return {};
-    const uri = `http://${this.ipaddress}:80/iZoneRequestV2`;
-    if (this.enableRespDebug) this.log(`getZonesInfo() ${uri} ${zone}`);
-    let respData = {};
-
-    const mapBody = { "iZoneV2Request": { "Type": 2, "No": zone, "No1": 0 } };
-
-    try {
-      respData.status = "failed";
-      const response = await axiosInstance.post(uri, JSON.stringify(mapBody), {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      respData = response.data;
-      if (respData.hasOwnProperty("ZonesV2")) respData.status = "ok";
-    } catch (e) {
-      if (this.enableRespDebug) this.log(`getZonesInfo() ERROR: ${e}`);
-      respData.status = "failed: " + e;
-    }
-    return respData;
+    const body = JSON.stringify({ iZoneV2Request: { Type: 2, No: zone, No1: 0 } });
+    return this.sendRawRequest('/iZoneRequestV2', body);
   }
 
   async getFirmwareList() {
-    if (!isValidIPAddress(this.ipaddress)) return {};
-    const uri = `http://${this.ipaddress}:80/iZoneRequestV2`;
-    if (this.enableRespDebug) this.log(`getFirmwareList() ${uri}`);
-    let respData = {};
-
-    const mapBody = { "iZoneV2Request": { "Type": 6, "No": 0, "No1": 0 } };
-
-    try {
-      respData.status = "failed";
-      const response = await axiosInstance.post(uri, JSON.stringify(mapBody), {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      respData = response.data;
-      if (respData.hasOwnProperty("Fmw")) respData.status = "ok";
-    } catch (e) {
-      if (this.enableRespDebug) this.log(`getFirmwareList() ERROR: ${e}`);
-      respData.status = "failed: " + e;
-    }
-    return respData;
+    const body = JSON.stringify({ iZoneV2Request: { Type: 6, No: 0, No1: 0 } });
+    return this.sendRawRequest('/iZoneRequestV2', body);
   }
 
   async sendSimpleiZoneCmd(cmd, value) {
-    if (!isValidIPAddress(this.ipaddress)) return {};
-    return this.sendSimpleUriCmdWithBody(
-      `http://${this.ipaddress}:80/iZoneCommandV2`,
-      JSON.stringify({ [cmd]: value }));
-  }
-
-  async sendSimpleUriCmdWithBody(uri, cmdbody) {
-
-    if (this.enableRespDebug) this.log(`sendSimpleUriCmdWithBody() uri: ${uri} cmdbody: ${cmdbody}`);
-
-    try {
-      const response = await axiosInstance.post(uri, cmdbody, {
-        responseType: 'text',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      return { status: response.data };
-    } catch (e) {
-      if (this.enableRespDebug) this.log(`sendSimpleUriCmdWithBody() ERROR: ${e}`);
-      return { status: `failed: ${e}` };
-    }
+    const body = JSON.stringify({ [cmd]: value });
+    return this.sendRawRequest('/iZoneCommandV2', body);
   }
 
   async sendMessageAndReturnAddress() {
     return new Promise((resolve, reject) => {
       const socket = dgram.createSocket('udp4');
-      let remoteAddress;
-
-      // Set up event listeners and binding only once
       socket.once('message', (message, remote) => {
-        this.log(`CLIENT RECEIVED: ${remote.address} : ${remote.port} - ${message}`);
-        remoteAddress = remote.address;
-        resolve(remoteAddress);
+        resolve(remote.address);
       });
-
-      socket.on('error', (err) => {
-        reject(err);
-      });
-
+      socket.on('error', reject);
       socket.bind(() => {
         socket.setBroadcast(true);
         socket.send('IASD', 0, 4, 12107, '255.255.255.255', (err) => {
-          if (err) {
-            reject(err);
-          }
+          if (err) reject(err);
         });
       });
-
-      // Close the socket after 1 second if no response is received
       this.homey.setTimeout(() => {
-        if (!remoteAddress) {
-          socket.close();
-          reject(new Error('No response received'));
-        }
+        socket.close();
+        reject(new Error('No response received'));
       }, 1000);
     });
   }
